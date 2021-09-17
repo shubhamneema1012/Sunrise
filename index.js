881a10ba6acdc8babd473b45b6eee38c83ffc937
shubham <shubhamneema1012@gmail.com>
2021-09-17 19:11:52 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,107 @@
-const chat=()=>{
-    alert("Now this feature not available.Please try after some time")
+let ad1=new Audio("./sounds/A4.mp3");
+let ad2=new Audio("./sounds/Ab4.mp3");
+let ad3=new Audio("./sounds/B4.mp3");
+let ad4=new Audio("./sounds/Bb4.mp3");
+let ad5=new Audio("./sounds/C4.mp3");
+let ad6=new Audio("./sounds/C5.mp3");
+let ad7=new Audio("./sounds/D4.mp3");
+let ad8=new Audio("./sounds/D5.mp3");
+let ad9=new Audio("./sounds/Db4.mp3");
+let ad10=new Audio("./sounds/Db5.mp3");
+let ad11=new Audio("./sounds/E4.mp3");
+let ad12=new Audio("./sounds/E5.mp3");
+let ad13=new Audio("./sounds/Eb4.mp3");
+let ad14=new Audio("./sounds/Eb5.mp3");
+let ad15=new Audio("./sounds/F4.mp3");
+let ad16=new Audio("./sounds/G4.mp3");
+let ad17=new Audio("./sounds/Gb4.mp3");
+const wh1=()=>{
+    ad1.play();  
+}
+const wh2=()=>{
+    ad2.play();
+}
+const wh3=()=>{
+    ad3.play();
+}
+const wh4=()=>{
+    ad4.play();
+}
+const wh5=()=>{
+    ad5.play();
+}
+const wh6=()=>{
+    ad6.play();
+}
+const wh7=()=>{
+    ad7.play();
+}
+const wh8=()=>{
+    ad8.play();
+}
+const wh9=()=>{
+    ad9.play();
+}
+const wh10=()=>{
+    ad10.play();
+}
+const wh11=()=>{
+    ad11.play();
+}
+const wh12=()=>{
+    ad12.play();
+}
+const wh13=()=>{
+    ad13.play();
+}
+const wh14=()=>{
+    ad14.play();
+}
+const wh15=()=>{
+    ad15.play();
+}
+const wh16=()=>{
+    ad16.play();
+}
+const wh17=()=>{
+    ad17.play();
+}
+const wh18=()=>{
+    ad1.play();
+}
+const wh19=()=>{
+    ad2.play();
+}
+const wh20=()=>{
+    ad3.play();
+}
+const bh1=()=>{
+    ad7.play();
+}
+const bh2=()=>{
+    ad8.play();
+}
+const bh3=()=>{
+    ad9.play();
+}
+const bh4=()=>{
+    ad10.play();
+}
+const bh5=()=>{
+    ad11.play();
+}
+const bh6=()=>{
+    ad12.play();
+}
+const bh7=()=>{
+    ad13.play();
+}
+const bh8=()=>{
+    ad14.play();
+}
+const bh9=()=>{
+    ad15.play();
+}
+const bh10=()=>{
+    ad16.play();
 }
